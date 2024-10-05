8d8a446bc620d8441da9e6591c04715242ae6527
JEDO139191 <jedo139191@gmail.com>
2024-10-05 08:47:07 -0600

    Jon Parcial II Progra II
    PrograII 0907 13 9191

---
+++ TiendaOnline/Handler.java
@@ -0,0 +1,11 @@
+package Parcial2Pogra2.TiendaOnline;
+
+abstract class Handler {
+    protected Handler nextHandler;
+
+    public void setNextHandler(Handler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    public abstract void handleRequest(String request);
+}
---
+++ TiendaOnline/PedidoEstandar.java
@@ -0,0 +1,40 @@
+package Parcial2Pogra2.TiendaOnline;
+
+
+
+class PedidoEstandar extends Handler {
+
+    @Override
+    public void handleRequest(String request) {
+        if (request.equals("Pedido Estandar")) {
+            System.out.println("Pedido Estandar: Procesando la solicitud.");
+            verificarPedido();
+        } else if (request.equals("Solicitud Aceptada")) {
+            System.out.println("Pedido Estandar: Solicitud Aceptada.");
+            verificarPago();
+        } else if (request.equals("Pago Aceptado")) {
+            System.out.println("Pedido Estandar: Pago Aceptado. Preparando el paquete.");
+            prepararPaquete();
+            System.out.println("Pedido Estandar: Paquete preparado. Envío en proceso.");
+            enviarPedido();
+        } else if (nextHandler != null) {
+            nextHandler.handleRequest(request);
+        }
+    }
+
+    private void verificarPedido() {
+        System.out.println("Pedido Estandar: Verificación del pedido en curso.");
+    }
+
+    private void verificarPago() {
+        System.out.println("Pedido Estandar: Verificación del pago en curso.");
+    }
+
+    private void prepararPaquete() {
+        System.out.println("Pedido Estandar: Preparación del paquete en curso.");
+    }
+
+    private void enviarPedido() {
+        System.out.println("Pedido Estandar: Envío exitoso.");
+    }
+}
